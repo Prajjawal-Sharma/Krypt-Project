17b4aabf5abb176da0a0313b74788f5c654b404a
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-03-23 03:19:23 +0530

    Updated Navbar

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,13 @@
+import { AiFillPlayCircle} from 'react-icons/ai';
+import {SiEthereum } from 'react-icons/si';
+import {BsInfoCircle} from 'react-icons/bs';
+
+import{ Loader } from './';
+
+
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+        <h1  >Welcome</h1>
     );
 
 }
