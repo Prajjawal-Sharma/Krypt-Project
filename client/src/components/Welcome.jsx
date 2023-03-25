a42a378151cb80e07f4b0ef9ead4afd1972cb965
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-03-25 23:39:16 +0530

    Updated Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,9 +6,16 @@ import{ Loader } from './';
 
 const commonStyles="min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input=({placeholder}) => (
+const Input=({placeholder,name,type,value,handleChange}) => (
+
     <input 
     placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e,name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparnt text-white border-none text-sm white-glassmorphism"
+
     />
 );
 
@@ -69,7 +76,7 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}}/>
                     </div>
                 </div>
 
