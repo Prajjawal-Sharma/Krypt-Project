2fa69520d5d03f95bf6a256ac98c4a401a9e2ba8
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-04-20 01:51:01 +0530

    updated smart_contract

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         )}
         {toggleMenu && (
           <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl  list-none
+            className="z-10 fixed -top-0 -right-2 p-3 w-[20rem] h-screen shadow-2xl  list-none
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
