1e28ba96eb1659062d8365995949ab34cb192ac9
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-06-07 00:17:33 +0530

    updated transaction history

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,30 @@
 import React, {useContext} from 'react';
 import {TransactionContext} from '../context/TransactionContext';
 import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress';
+
+const TransactionCard =({addressTo,addressFrom,timestamp,message,keyword,amount,url}) => {
+    return(
+        <div className='bg-[#181918] m-4 flex flex-1 
+        2xl:min-w-[450px] 
+        2xl:max-w-[500px]
+        sm:min-w-[270px] 
+        sm:max-w-[300px]
+        flex-col p-3 rounded-md hover:shadow-2xl'>
+            <div className='flex flex-xol items-center w-full mt-3'>
+                <div className='flex justify-start w-full mb-6 p-2'>
+                    <a href={`https://sepolia.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer">
+                        <p className='text-white text-base'>From: {shortenAddress(addressFrom)} </p>
+
+                    </a>
+
+                </div>
+
+            </div>
+        </div>
+    ) 
+    
+}
 
 const Transactions = () => {
     const { currentAccount } = useContext(TransactionContext);
@@ -12,7 +36,11 @@ const Transactions = () => {
                 ) : (
                     <h3 className='text-white text-3xl text-center my-2'>Connect your account to see the latest changes</h3>
                 )}
-               
+               <div className='flex flex-wrap justify-center items-center mt-10'>
+                {dummyData.reverse().map((Transaction, i ) => (
+                    <TransactionCard key={i} {...Transaction}/>
+                ))}
+               </div>
             </div>
         </div>
     );
